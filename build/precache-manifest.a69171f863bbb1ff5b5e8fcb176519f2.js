9d2ccdeace9fa3bf34da010ec9e76d479f429b1b
amazona <docongminh53@gmail.com>
2020-05-30 16:10:47 +0700

    commit

---
+++ build/precache-manifest.a69171f863bbb1ff5b5e8fcb176519f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f2b3dfed27ad950faee123e07be70a",
+    "url": "/docongminh1/amazona/index.html"
+  },
+  {
+    "revision": "ae6c7881545f3156c293",
+    "url": "/docongminh1/amazona/static/css/main.3f289290.chunk.css"
+  },
+  {
+    "revision": "7cd41a1f707a76cbcb8e",
+    "url": "/docongminh1/amazona/static/js/2.743ecd45.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/docongminh1/amazona/static/js/2.743ecd45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6c7881545f3156c293",
+    "url": "/docongminh1/amazona/static/js/main.6b3b55b7.chunk.js"
+  },
+  {
+    "revision": "8c65a17d6b71a66cf93c",
+    "url": "/docongminh1/amazona/static/js/runtime-main.67d838f3.js"
+  }
+]);
